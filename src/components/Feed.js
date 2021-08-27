10c9295d67930c32c54f8c1b135a22147d60f20d
Pearl Arora <ipearlarora@gmail.com>
2021-08-28 01:38:51 +0530

    feed and popup

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./css/Feed.css";
+import Popup from "./Popup";
+
+import { Avatar } from "@material-ui/core";
+import me from "../components/css/assets/me.jpg";
 
 function Feed() {
   return (
     <div className="feed">
-
+      <div className="start-post">
+        <Avatar src={me} className="startPost-avatar" />
+        <button>Start a post</button>
+      </div>
+      <div className="startPost-popup">
+        <Popup trigger={true}>
+          <h1>popup triggered</h1>
+        </Popup>
+      </div>
     </div>
   );
 }
