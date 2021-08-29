4441ad6be509927aa1656a3115f9a29a34393259
Pearl Arora <ipearlarora@gmail.com>
2021-08-30 04:05:49 +0530

    redux setup

---
+++ src/features/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: null,
+  },
+
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+// Selectors - To pull the information
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducer;
